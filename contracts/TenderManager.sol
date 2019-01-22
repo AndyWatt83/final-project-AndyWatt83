020352bd56956a05322f45f4916a849126e51e1f
Andy Watt <andy.watt83@gmail.com>
2019-01-22 18:47:25 +0000

    Starting to flesh out the TenderManager contract

--- contracts/TenderManager.sol
+++ contracts/TenderManager.sol
@@ -1,9 +1,21 @@
 pragma solidity ^0.5.0;
 
+/// @author Andy Watt
+/// @title The manager contract for the decentralised tendering application
 contract TenderManager
 {
+    mapping (address => bool) public registeredClient;
+    mapping (address => bool) public registeredBidder;
+    mapping (uint => address) public jobOwners;
+
+    event NewClientRegistered(address indexed clientAddress);
+    event NewBidderRegistered(address indexed bidderAddress);
+
+    uint currentJobId;
+
     constructor () public
     {
         // TODO
     }
+
 }
