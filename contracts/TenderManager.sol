91bba6f25d7c3ef80a90a331956e521af6ef7c7b
Andy Watt <andy.watt83@gmail.com>
2019-01-23 00:19:51 +0000

    Commenting

--- contracts/TenderManager.sol
+++ contracts/TenderManager.sol
@@ -19,24 +19,32 @@ contract TenderManager is Ownable, Pausable
     event NewClientRegistered(address indexed clientAddress);
     event NewBidderRegistered(address indexed bidderAddress);
 
+    /// Modifier which ensures that the sender is registerd as a client.
+    /// @dev Checks that the sender address has an entry in registeredClients.
     modifier callerIsClient()
     {
         require (registeredClients[msg.sender], "Caller is not a registered client");
         _;
     }
 
+    /// Modifier which ensures that the sender currently has an open tender.
+    /// @dev Checks that the id associated with the sender is greater than zero.
     modifier clientHasOpenTender()
     {
         require (clientTenderIds[msg.sender] > 0, "Client does not have an open tender");
         _;
     }
 
+    /// Modifier which ensures that the sender does not currently have an open tender.
+    /// @dev Checks that the id associated with the sender is zero.
     modifier clientHasNoOpenTender()
     {
         require (clientTenderIds[msg.sender] == 0, "Client has a tender open already");
         _;
     }
 
+    /// Constructor for the TenderManager contract.
+    /// @dev Takes no arguments, sets the currentJobId to 1 (zero is used to mean null).
     constructor () public
     {
         currentJobId = 1;
