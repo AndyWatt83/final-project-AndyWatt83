a67a0281e516a152621dcee63bee6cc91094d02e
Andy Watt <andy.watt83@gmail.com>
2019-01-23 00:21:44 +0000

    Remove the Tender contract from the deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,7 @@
 const Configuration = artifacts.require('./Configuration.sol');
 const TenderManager = artifacts.require('./TenderManager.sol');
-const Tender = artifacts.require('./Tender.sol');
 
 module.exports = function (deployer) {
     deployer.deploy(Configuration);
     deployer.deploy(TenderManager);
-    deployer.deploy(Tender);
 };
